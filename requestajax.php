f7fc417909e24389e4c92e590bde3c92d04046c3
Maxgoods <maxgoods62@gmail.com>
2017-02-16 15:48:15 +0100

    Sanitize requetejax

--- requestajax.php
+++ requestajax.php
@@ -5,33 +5,36 @@ include("cardsinfo.php");
 
 
 
-if (empty($_POST['sort'])) {
+if (empty($_GET['sort']) || !is_numeric($_GET['startIndex']) || !is_numeric($_GET['size'])) {
     exit();
 }
-$sort = htmlspecialchars($_POST['sort']);
+$sort = htmlspecialchars($_GET['sort']);
+$startIndex = $_GET['startIndex'];
+$size = $_GET['size'];
+
 
 if ($sort == "hot") {
 
-    $req = $bdd->query ('SELECT id FROM images ORDER BY nb_vote_positif LIMIT '.$_POST['startIndex']. ',' . ( $_POST['startIndex'] + $_POST['size'] ));
+    $req = $bdd->query ('SELECT nom_hash FROM images ORDER BY nb_vote_positif LIMIT ' . $startIndex . ',' .  $size );
 
     while ($resultat = $req->fetch()) {
-        echo getInfo(sha1($resultat["id"].SALT_ID)).'</br>';
+        echo getInfo($resultat["nom_hash"]).'</br>';
     }
 
 } elseif ($sort == "new") {
 
-    $req = $bdd->query ('SELECT id FROM images ORDER BY date_creation DESC LIMIT '.$_POST['startIndex']. ',' . ( $_POST['startIndex'] + $_POST['size'] ));
+    $req = $bdd->query ('SELECT nom_hash FROM images ORDER BY date_creation DESC LIMIT ' . $startIndex . ',' . $size );
 
     while ($resultat = $req->fetch()) {
-       echo getInfo(sha1($resultat["id"].SALT_ID)).'</br>';
+       echo getInfo($resultat["nom_hash"]).'</br>';
     }
 
 } elseif ($sort == "random") {
 
-    $req = $bdd->query ('SELECT id FROM images ORDER BY RAND() DESC LIMIT '.$_POST['size'] );
+    $req = $bdd->query ('SELECT nom_hash FROM images ORDER BY RAND() DESC LIMIT ' . $size );
 
     while ($resultat = $req->fetch()) {
-        echo getInfo(sha1($resultat["id"].SALT_ID)).'</br>';
+        echo getInfo($resultat["nom_hash"]).'</br>';
     }
 }
 
